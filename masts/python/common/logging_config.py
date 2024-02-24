deb680fc433659f2a2048a739e3f4d463e2cbb52
lucasmartinoia <lucas.martinoia@gmail.com>
2024-02-24 17:53:31 +0100

    Add function name and line to logging

--- masts/python/common/logging_config.py
+++ masts/python/common/logging_config.py
@@ -1,7 +1,7 @@
 import logging
 
 def setup_logging():
-    logging.basicConfig(filename='../smart_trader.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename='../smart_trader.log', level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(funcName)s line %(lineno)d: %(message)s')
 
 # Create a logger object
 logger = logging.getLogger(__name__)
